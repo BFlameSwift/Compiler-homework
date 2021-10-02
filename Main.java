6afe9d6f98595265ef1618d5eae0850f2c39cb16
BFlameSwift <19373732@buaa.edu.cn>
2021-10-02 15:38:09 +0800

    change style

--- Main.java
+++ Main.java
@@ -14,18 +14,12 @@ public class Main {
     public static List<String> reservedWordsAfter = Arrays.asList("If","Else","While","Break","Continue","Break","Return");
     public static List<String> signBefore = Arrays.asList("==","=",";","(",")","{","}","+","*","/","<",">");
     public static List<String> signAfter = Arrays.asList("Eq","Assign","Semicolon","LPar","RPar","LBrace","RBrace","Plus","Mult","Div","Lt","Gt");
-
-
-
-    public static void main(String[] args) {
+    
+    public static void main(String[] args) throws FileNotFoundException{
         String filePath = args[0];
         ArrayList<String> words = new ArrayList<String>();
         Scanner scanner = null;
-        try {
-            scanner = new Scanner(new File(filePath));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        scanner = new Scanner(new File(filePath));
         boolean hasError = false;
         while (scanner.hasNextLine()&&!hasError)//逐行读取文件内容
         {
@@ -49,7 +43,7 @@ public class Main {
                 Pattern pattern = Pattern.compile(BLANK);
                 if(pattern.matcher(thisStr).matches()){
                     continue;
-                }
+                }// 跳过空白
                 //TODO System.out.println(thisStr);
                 words.add("----");
                 return true;
@@ -58,12 +52,11 @@ public class Main {
 
             if(isIdentifier(thisStr)){
                 for(j = i+1;j<lineLen;j++){
-                    if( ! (line.charAt(j)>=97&&line.charAt(j)<=122 || line.charAt(j)>=48 && line.charAt(j) <= 57 || line.charAt(j) >= 65 && line.charAt(j) <= 90 || line.charAt(j) == '_'))
+                    if( ! (line.charAt(j)>='a'&&line.charAt(j)<='z' || line.charAt(j)>='0' && line.charAt(j) <= '9' || line.charAt(j) >= 'A' && line.charAt(j) <= 'Z' || line.charAt(j) == '_'))
                         break;
                 }
             }
             else if (isSymbol(thisStr)){
-
                 if(line.charAt(j) == '='){
                     if (j < lineLen-1 && line.charAt(j+1) == '='){
                         j++;
@@ -77,12 +70,10 @@ public class Main {
                         break;
                 }
             }
-
             if (j>i)  {
                 words.add(line.substring(i,j));
                 i = j-1;
             }
-
         }
         return false;
     }
