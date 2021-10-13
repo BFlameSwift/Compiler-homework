298cbe8691624856697136d52639de7aec2c2328
BFlameSwift <19373732@buaa.edu.cn>
2021-10-13 23:42:16 +0800

    init 40 Const

--- lab01/Lexical.java
+++ lab01/Lexical.java
@@ -20,93 +20,38 @@ public class Lexical {
     public static List<String> signBefore = Arrays.asList("==","=",";","(",")","{","}","[","]","+","*","/","<",">");
     public static List<String> signAfter = Arrays.asList("Eq","Assign","Semicolon","LPar","RPar","LBrace","RBrace","LSqure","RSqure","Plus","Mult","Div","Lt","Gt");
 
+    public static final int
+        CONST_DEC = 1,INT_DEC = 2,VOID_DEC = 3,IF_DEC = 4,ELSE_DEC = 5,WHILE_DEC = 6,CONTINUE_DEC = 7,BREAK_DEC =8, RETURN_DEC = 9,
+        ASSIGN = 10,SEMICOLON = 11,LPAREN = 12,RPAREN = 13,LBRACKET = 14,RBRACKET = 15,LBRACE = 16,RBRACE = 17,LT = 18,GT = 19,LE = 20,GE = 21,
+        IDENT = 22,DECIMAL_CONST = 23,
+        OCTAL_CONST = 24,HEXADECIMAL_CONST = 25,
+        PLUS = 26,MINUS = 27,MUL = 28,DIV = 29,MOD = 30, NOT = 31,EQUALS = 32,NEQUALS = 33,COMMA = 34,
+        SINGLE_QUOTE = 35,DOUBLE_QUOTE = 36,SINGLE_COMMENT = 37,BLOCK_COMMENT = 38,AND = 39,OR = 40;
+    public static final String[] SYMBOL_LIST = {
+            null,"const","int","void","if","else","while","continue","break","return",
+            "=",";","(",")","[","]","{","}","<",">","<=",">=",
+            null,null,null,null,
+            "+","-","*","/","%","!","==","!=", "," ,
+            "'","\"","\\\\",null,"&&","||"
+    };
+    public static final String[] TOKEN_NAME = {
+            null,"Const","Int","Void" ,"If" ,"Else" ,"While" ,"Continue" ,"Break", "Return",
+            "Assign","Semicolon","LParen","RParen","LBracket","RBracket","LBrace","RBrace","Lt","Gt","Le","Ge",
+            "Ident","Decimal","Octal","Hexadecimal",
+            "Plus" ,"Minus" ,"Mul","Div","Mod", "Not","Equals","NEquals","Comma",
+            "SingleQuote" ,"DoubleQuote" ,"SingleComment" ,"BlockComment","And","Or"
+    };
 
+
+
+
+    
 
     public static void main(String[] args) throws FileNotFoundException {
-        String filePath = args[0];
-        ArrayList<String> words = new ArrayList<String>();
-        Scanner scanner = null;
-        scanner = new Scanner(new File(filePath));
-        boolean hasError = false;
-        while (scanner.hasNextLine()&&!hasError)//逐行读取文件内容
-        {
-            String line = scanner.nextLine();
-            hasError = processLine(line,words);
-        }
-        for (String word : words){
-//            System.out.println(word+" "+word.length()+"  "+ tokenTran(word));
-            System.out.println(tokenTran(word));
+        for (int i=0;i<SYMBOL_LIST.length;i++){
+            System.out.println(i+"   "+SYMBOL_LIST[i]+"   "+TOKEN_NAME[i]);
         }
     }
-    public static Boolean processLine(String line,ArrayList<String> words){
-        line = line.strip();
-//        String[] strList = line.split("\\s+");
-        int lineLen = line.length(),i,j,k;
-        for (i =0;i<lineLen;i++){
-            j = i;
-            String thisStr = line.substring(i,i+1);
-            if(!isLegalWord(thisStr)) {
-                Pattern pattern = Pattern.compile(BLANK);
-                if(pattern.matcher(thisStr).matches()){
-                    continue;
-                }// 跳过空白
-                //TODO System.out.println(thisStr);
-                words.add("----");
-                return true;
-            }
-            if(isIdentifier(thisStr)){
-                for(j = i+1;j<lineLen;j++){
-                    if( ! (line.charAt(j)>='a'&&line.charAt(j)<='z' || line.charAt(j)>='0' && line.charAt(j) <= '9' || line.charAt(j) >= 'A' && line.charAt(j) <= 'Z' || line.charAt(j) == '_'))
-                        break;
-                }
-            }
-            else if (isSymbol(thisStr)){
-                if(line.charAt(j) == '='){
-                    if (j < lineLen-1 && line.charAt(j+1) == '='){
-                        j++;
-                    }
-                }j++;
-            }
-            else if(Number.isNumber(thisStr)){
-                for(j = i+1;j<lineLen;j++){
-                    if( ! (line.charAt(j) >= 48 && line.charAt(j) <= 57))
-                        break;
-                }
-            }
-            if (j>i)  {
-                words.add(line.substring(i,j));
-                i = j-1;
-            }
-        }return false;
-    }
-    public static String tokenTran(String str){
-        String token;
-        int index = -1;
-        if( isReservedWord(str)) {
-//            System.out.println(str);
-            index = reservedWordsBefore.indexOf(str);
-            token = reservedWordsAfter.get(index);
-        }
-        else if( isSymbol(str)){
-            index = signBefore.indexOf(str);
-            token = signAfter.get(index);
-        }
-        else if (Number.isNumber(str)){return "Number("+ str +")";}
-        else if (isIdentifier(str)){return "Ident("+str+")";}
-        else {return "Err";}
-        return token;
-    }
-
-
-
-
-    public static boolean isIdentifier(String str){
-        Pattern pattern = Pattern.compile(IDENTIFIER);
-        return pattern.matcher(str).matches();
-    }
-    public static boolean isReservedWord(String str){return reservedWordsBefore.contains(str);}
-    public static boolean isSymbol(String str){return signBefore.contains(str);}
-    public static boolean isLegalWord(String str){return isReservedWord(str) || isIdentifier(str) || isSymbol(str); }
 }
 
 
