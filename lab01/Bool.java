39e8f5d30a89f43c2b15247e7390ddaafffed84b
BFlameSwift <19373732@buaa.edu.cn>
2021-10-14 15:27:08 +0800

    update some big

---
+++ lab01/Bool.java
@@ -0,0 +1,25 @@
+package lab01;
+import Word.*;
+import Word.Number;
+
+/**
+ * @author Bflame
+ */
+public class Bool {
+    public static Boolean isFuncType(String str){
+        if (str.equals("void") || str.equals("int")){
+            return true;
+        }
+        return false;
+    }
+    public static Boolean Statements(String str){
+        if (Number.isNumber(str)){
+            return true;
+        else if (str.equals("return")){
+            return true;
+        }
+        // 分号结尾
+        return false;
+
+}
+
