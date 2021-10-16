3b4572820231db2708b977f7480531517f66a67d
BFlameSwift <19373732@buaa.edu.cn>
2021-10-16 16:00:02 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,8 +8,13 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(args[0]);
-       int a = scanner.nextInt(),b = scanner.nextInt();
+        int a,b;
+        if(scanner.hasNext()){
+            a = scanner.nextInt();b = scanner.nextInt();
+            System.out.println(a+b);
+            System.exit(0);
+        }
+
         System.out.println(a+b);
         System.exit(0);
     }
