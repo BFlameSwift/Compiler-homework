9d9f32773bb3a02605849c8ee2a66fe1809ddde0
BFlameSwift <19373732@buaa.edu.cn>
2021-10-16 15:53:09 +0800

    init test

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author BFlame
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+       int a = scanner.nextInt(),b = scanner.nextInt();
+        System.out.println(a+b);
+
+    }
+
+}
+
+
