58aedc1b060946b888ab57df29b37318edabdbc5
BFlameSwift <19373732@buaa.edu.cn>
2021-10-16 16:02:08 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a,b;
+        int a=1,b=2;
         if(scanner.hasNext()){
             a = scanner.nextInt();b = scanner.nextInt();
             System.out.println(a+b);
