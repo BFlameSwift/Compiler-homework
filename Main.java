988d225fe448265a07ab2377d19df9870d1c68a5
BFlameSwift <19373732@buaa.edu.cn>
2021-10-16 16:05:56 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,14 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 /**
  * @author BFlame
  */
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         int a=1,b=2;
-        if(scanner.hasNext()){
-            a = scanner.nextInt();b = scanner.nextInt();
-            System.out.println(a+b);
-            System.exit(0);
-        }
-
+// 因为好看才加的。。之前叫错了是红色的很难受。。
         System.out.println(a+b);
         System.exit(0);
     }
-
 }
 
 
