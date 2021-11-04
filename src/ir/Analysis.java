69bc46ad8c3cbfbf2853454d341a46290a4184dc
BFlameSwift <19373732@buaa.edu.cn>
2021-11-04 19:58:15 +0800

    ready to judge

---
+++ src/ir/Analysis.java
@@ -0,0 +1,11 @@
+package ir;
+
+import java.util.ArrayList;
+
+/**
+ * @author ly
+ * @date 2021/11/3 16:00
+ */
+public class Analysis {
+    public ArrayList<String> midCode = new ArrayList<String>();
+}
